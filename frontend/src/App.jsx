cc95ff4ac809dfe127c0b166c1e7984bd2c93223
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-08-17 20:19:33 +0700

    add a loading animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import TypewriterText from './components/UI/TypewriterText';
 import profile from "../src/assets/profile.jpg"
 import { FaUser, FaCode, FaBriefcase, FaStar, FaCog, FaGlobe, FaHome , FaRocket} from 'react-icons/fa';
 import './styles/layout/AppLayout.css';
+import "./loading.css"
 const AboutSection = lazy(() => import('./components/Sections/AboutSection'));
 const ProjectsSection = lazy(() => import('./components/Sections/ProjectsSection'));
 const ExperienceSection = lazy(() => import('./components/Sections/ExperienceSection'));
@@ -59,18 +60,81 @@ const [isLoading, setIsLoading] = useState(true);
   ];
 
   if (isLoading) {
-    return (
-      <div className="app-container">
-        <div className="loading-screen">
-          <div className="loading-card">
-            <FaRocket className="loading-icon" />
-            <h2>Loading Portfolio</h2>
-            <LoadingDots />
+  return (
+    <div id="totalbase">
+      <div id="animbase">
+        <div id="base">
+          <div id="baseshadow"></div>
+          <div className="helitop">
+            <div className="wingbase">
+              <div className="col1"></div>
+              <div className="col2"></div>
+              <div className="col3"></div>
+              <div className="col4"></div>
+              <div className="wing1"></div>
+              <div className="wing2"></div>
+            </div>
           </div>
+          <div className="helifront">
+            <div className="tailbase">
+              <div className="tail1">
+                <div className="tail21"></div>
+                <div className="tail22"></div>
+                <div className="tail23"></div>
+                <div className="tail24">
+                  <div className="tailwingbase">
+                    <div className="tail221"></div>
+                    <div className="tail222"></div>
+                    <div className="tail223"></div>
+                    <div className="tail224"></div>
+                    <div className="tailwing1"></div>
+                    <div className="tailwing2"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="tail25"></div>
+              <div className="tail26"></div>
+            </div>
+          </div>
+          <div className="heliback">
+            <div className="front1"></div>
+            <div className="front2"></div>
+            <div className="front3"></div>
+            <div className="front4"></div>
+            <div className="front5"></div>
+          </div>
+          <div className="helileft"></div>
+          <div className="heliright"></div>
+          <div className="helibottom"></div>
         </div>
       </div>
-    );
-  }
+      <div id="towerbase">
+        <div id="talkbubble" align="center">
+          airport is<br /> loading...<br />please wait..
+        </div>
+        <div className="tower1left"></div>
+        <div className="tower1right"></div>
+        <div className="tower2right">
+          <div className="tower1window"></div>
+        </div>
+        <div className="tower1top">
+          <div className="front1">
+            <div className="col1"></div>
+            <div className="col2"></div>
+            <div className="col3"></div>
+            <div className="col4"></div>
+          </div>
+          <div className="front2"></div>
+          <div className="front3"></div>
+        </div>
+        <div className="tower2top"></div>
+        <div className="tower2left">
+          <div className="tower2window"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
   return (
     <div className="app-container">
